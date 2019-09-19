0a14d844d4cff24c95c18f7dc8a859f6924087f8
Andrey Zagoruy <andreyzagoruy@Andreys-MBP.lan>
2019-09-19 18:43:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fruits test app
+Can be tested online at [Codepen](https://codepen.io/Zagoruy/pen/oNvQbQa)
---
+++ js/main.js
@@ -0,0 +1,47 @@
+const SELECTED_FRUIT = "banana";
+const WORD_TO_PREPEND = "Only";
+
+function isEquals(first, second) {
+  return first === second ? true : false;
+}
+
+function isElement(element) {
+  const result = element instanceof Element || element instanceof HTMLDocument;
+  if (!result) {
+    console.warn("You must provide valid HTML Element");
+  }
+  return result;
+}
+
+function prependText(initialString, textToPrepend) {
+  return textToPrepend + " " + initialString;
+}
+
+function createElement(value, elementType) {
+  if (!elementType) {
+    elementType = "li";
+  }
+  const element = document.createElement(elementType);
+  element.innerHTML = value;
+  return element;
+}
+
+function appendTo(containerName, element) {
+  const selectedContainer = document.querySelector(containerName);
+
+  if (!containerName || !selectedContainer || !element || !isElement(element)) {
+    return;
+  }
+
+  return selectedContainer.appendChild(element);
+}
+
+const fruits = ["apple", "banana", "orange", "melon"];
+
+fruits.forEach(fruit => {
+  if (isEquals(fruit, SELECTED_FRUIT)) {
+    fruit = prependText(fruit, WORD_TO_PREPEND);
+  }
+  const element = createElement(fruit);
+  appendTo("#jsFruitsContainer", element);
+});
